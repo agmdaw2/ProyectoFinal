c68dc04d4088a7076f88f8ed6eee46169012b9da
IvanDominguez2000 <71886812+IvanDominguez2000@users.noreply.github.com>
2021-06-02 15:53:24 +0200

    IDP-Lista Actividades

--- Proyecto/Vagrant/html/listaActividades.php
+++ Proyecto/Vagrant/html/listaActividades.php
@@ -1,45 +1,39 @@
-<?php
-  session_start();
-  if(isset($_SESSION["usuario"])){
-  }
-?>
-
 <!DOCTYPE html>
+
 <html>
+
 <head>
   <title>Tecnoetica</title>
   <link rel="stylesheet" type="text/css" href="css/main2.css">
   <link rel="stylesheet" type="text/css" href="css/main.css">
   <meta charset="UTF-8">
+  <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
 </head>
+
 <body>
+
   <div id="pagina">
     <div id="cabecera">
-      <a href="index.php"><img src="img/logotipo.png" alt="logo" width="300px" height="90px"></a>
+      <a href="Login.html"><img src="img/logotipo.png" alt="logo" width="300px" height="90px"></a>
     </div>
     <div class="Login-Registro">
-      <a href="Login.php"><img src="img/Perfil.png" alt="Perfil" width="50px" height="50px"></a>
-            <?php
-                if(isset($_SESSION["usuario"])){
-                    echo'<a href="logout.php"><img src="img/logout.png" alt="Logout" width="50px" height="50px"></a>';
-                }
-            ?>
+      <img src="img/Perfil.png" alt="Perfil" width="50px" height="50px">
     </div>
     <div class="navbar">
       <div class="subnav">
-        <button class="subnavbtn" onclick="window.location.href='index.php'">Inicio <i class="fa fa-caret-down"></i></button>
+        <button class="subnavbtn" onclick="window.location.href='Inicio.html'">Inicio <i class="fa fa-caret-down"></i></button>
       </div>
       <div class="subnav">
-        <button class="subnavbtn" onclick="window.location.href='listaActividades.php'">Dilemas Tecnoeticos<i class="fa fa-caret-down"></i></button>
+        <button class="subnavbtn" onclick="window.location.href='Productes.html'">Dilemas Tecnoeticos<i class="fa fa-caret-down"></i></button>
       </div>
       <div class="subnav">
-        <button class="subnavbtn" onclick="window.location.href='propuestaDidactica.php'">Propuesta Didactica<i class="fa fa-caret-down"></i></button>
+        <button class="subnavbtn" onclick="window.location.href='Nosotros.html'">Propuesta Didactica<i class="fa fa-caret-down"></i></button>
       </div>
       <div class="subnav">
-        <button class="subnavbtn" onclick="window.location.href='tecnoeticaFutura.php'">Tecnoetica Futura<i class="fa fa-caret-down"></i></button>
+        <button class="subnavbtn" onclick="window.location.href='Exercicis.html'">Tecnoetica Futura<i class="fa fa-caret-down"></i></button>
       </div>
       <div class="subnav">
-        <button class="subnavbtn" onclick="window.location.href='contacto.php'">Contacto<i class="fa fa-caret-down"></i></button>
+        <button class="subnavbtn" onclick="window.location.href='Blabla.html'">Contacto<i class="fa fa-caret-down"></i></button>
       </div>
     </div>
     <hr>
@@ -61,9 +55,11 @@
 
       $sql = "SELECT titulo_dilema FROM dilema;";
 
+
       if ($result = $mysqli->query($sql)) {
+        $count = 1;
         while ($row = $result->fetch_row()) {
-          echo ('<div id="dilema1">' . $row[0] . '</div>');
+          echo ('<div id="dilema' . $count . '" class="titulo" onMouseEnter="mouseOver('. $count++ .')">' . $row[0] . '</div>');
         }
       }
       $mysqli->close();
@@ -71,25 +67,64 @@
     </div>
 
     <div id="dilemaSeleccionado">
-      <?php
-      $mysqli = new mysqli("localhost", "root", "password", "tecnoticos");
-      if ($mysqli->connect_errno) {
-        echo "Failed to connect to MySQL: " . $mysqli->connect_error;
-        exit();
-      }
-
-      $sql = "SELECT resumen_dilema FROM dilema where id_dilema=1";
-
-      if ($result = $mysqli->query($sql)) {
-        while ($row = $result->fetch_row()) {
-          echo ('<p>' . $row[0] . '<p>');
+      <div id="resumenes">
+        <?php
+        $mysqli = new mysqli("localhost", "root", "password", "tecnoticos");
+        if ($mysqli->connect_errno) {
+          echo "Failed to connect to MySQL: " . $mysqli->connect_error;
+          exit();
         }
-      }
-      $mysqli->close();
-      ?>
+
+        $sql = "SELECT resumen_dilema FROM dilema";
+        $sql2 = "SELECT id_dilema FROM dilema";
+
+        $resultResumen = $mysqli->query($sql);
+        if ($result = $mysqli->query($sql)) {
+          while ($row = $result->fetch_row()) {
+            //echo ('<div id="resumenDilema"><p>' . $row[0] . '<p></div>');   
+            echo ($row[0] . "\n");
+          }
+        }
+        $mysqli->close();
+        ?>
+      </div>
     </div>
 
-    <div class="Footer">
+    <script>
+      var divs = document.getElementsByClassName("titulo");
+      var numero = 0
+
+      for (var i = 0; i < divs.length; i++) {
+      //  var a = i;
+      //  divs[i].addEventListener("mouseenter", function() {
+      //    mouseOver(a);
+      //  });
+          divs[i].addEventListener("mouseleave", function() {
+            mouseOut();
+          });
+      }
+
+      function mouseOver(numero) {
+        document.getElementById("resumenes").style.backgroundColor = "black";
+        document.getElementById("resumenes").style.color = "white";
+        document.getElementById("dilemaSeleccionado").style.backgroundColor = "black";
+        document.getElementById("dilemaSeleccionado").style.color = "white";
+
+        document.getElementById("dilemaSeleccionado").innerHTML = '<div id="resumenes" display="none">' +
+          document.getElementById("resumenes").innerHTML +
+          '</div>' +
+          document.getElementById("resumenes").innerHTML.split("\n")[numero];
+      }
+
+      function mouseOut() {
+        document.getElementById("resumenes").style.backgroundColor = "white";
+        document.getElementById("resumenes").style.color = "black";
+        document.getElementById("dilemaSeleccionado").style.backgroundColor = "white";
+        document.getElementById("dilemaSeleccionado").style.color = "black";
+      }
+    </script>
+
+    <div class="Footer" style="margin-top: 10%;">
       <div id="footerContacto" class="ContenidoFooter">Correo:</div>
       <div id="footerCopy" class="ContenidoFooter">2021-2022 &copy; Tecnoetica</div>
       <div id="footerRedes" class="ContenidoFooter">Twitter</div>
